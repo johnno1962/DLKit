6e62ee9d06850aeb97814c962d2a7becd886f93b
John Holdsworth <github@johnholdsworth.com>
2025-10-30 22:05:10 +0100

    Compatibility with SwiftTrace.

--- Sources/DLKit/Demangling.swift
+++ Sources/DLKit/Demangling.swift
@@ -6,32 +6,33 @@
 //  Copyright © 2020 John Holdsworth. All rights reserved.
 //
 //  Repo: https://github.com/johnno1962/DLKit
-//  $Id: //depot/DLKit/Sources/DLKit/Demangling.swift#5 $
+//  $Id: //depot/DLKit/Sources/DLKit/Demangling.swift#6 $
 //
 
 #if DEBUG || !DEBUG_ONLY
 #if canImport(Darwin)
 import Foundation
 
+@_silgen_name("swift_demangle")
+private
+func _stdlib_demangleImpl(
+    _ mangledName: UnsafePointer<CChar>?,
+    mangledNameLength: UInt,
+    outputBuffer: UnsafeMutablePointer<UInt8>?,
+    outputBufferSize: UnsafeMutablePointer<UInt>?,
+    flags: UInt32
+    ) -> UnsafeMutablePointer<CChar>?
+
+@_silgen_name("__cxa_demangle")
+private
+func __cxa_demangle(_ mangled_name: UnsafePointer<Int8>?,
+                    output_buffer: UnsafePointer<Int8>?,
+                    length: UnsafeMutablePointer<size_t>?,
+                    status: UnsafeMutablePointer<Int32>?)
+    -> UnsafeMutablePointer<Int8>?
+
 /// Extension for easy demangling of Swift symbols
 public extension DLKit.SymbolName {
-    @_silgen_name("swift_demangle")
-    private
-    func _stdlib_demangleImpl(
-        _ mangledName: UnsafePointer<CChar>?,
-        mangledNameLength: UInt,
-        outputBuffer: UnsafeMutablePointer<UInt8>?,
-        outputBufferSize: UnsafeMutablePointer<UInt>?,
-        flags: UInt32
-        ) -> UnsafeMutablePointer<CChar>?
-
-    @_silgen_name("__cxa_demangle")
-    func __cxa_demangle(_ mangled_name: UnsafePointer<Int8>?,
-                        output_buffer: UnsafePointer<Int8>?,
-                        length: UnsafeMutablePointer<size_t>?,
-                        status: UnsafeMutablePointer<Int32>?)
-        -> UnsafeMutablePointer<Int8>?
-
     /// return Swif tlanguage description of symbol
     var demangled: String? {
         if let demangledNamePtr = _stdlib_demangleImpl(
